6ea6862f883563d9801937f8f055e066744e5c9c
jeongminju0815 <jmju0815@gmail.com>
2023-05-29 23:51:57 +0900

    5.29 조합

---
+++ solved/brute_force/B3040.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+arr = []
+
+for i in range(9):
+    arr.append(int(input()))
+
+for i in combinations(arr,7):
+    if sum(i) == 100:
+        temp = i
+    
+for i in temp:
+    print(i)
